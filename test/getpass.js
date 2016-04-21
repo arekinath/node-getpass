11419e2e861be30c818f422e7c3cb256cc222003
Alex Wilson <alex.wilson@joyent.com>
2016-04-21 15:56:15 -0700

    Fix up node 0.8 and 0.10 crashes

--- test/getpass.js
+++ test/getpass.js
@@ -9,6 +9,8 @@
 const mod_getpass = require('../lib/index');
 
 mod_getpass.getPass(function (err, password) {
-	var pwb64 = (new Buffer(password, 'utf8')).toString('base64');
+	var pwb64;
+	if (!err)
+		pwb64 = (new Buffer(password, 'utf8')).toString('base64');
 	console.log(JSON.stringify({error: err, password: pwb64}));
 });
